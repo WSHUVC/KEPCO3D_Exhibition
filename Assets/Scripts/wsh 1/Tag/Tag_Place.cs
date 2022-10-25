d35a800b1ecb155912dc89c1f3ce1b8765c4fbac
WSHUVC <wsh@uvc.co.kr>
2022-10-25 20:01:42 +0900

    Modify 1. Camera Action Bug

--- Assets/Scripts/wsh 1/Tag/Tag_Place.cs
+++ Assets/Scripts/wsh 1/Tag/Tag_Place.cs
@@ -12,6 +12,11 @@ namespace WSH.UI
         _765kVGIS,
         태양열발전현황
     }
+
+    //0:012
+    //1:345
+    //2:678
+    //3:91011
     public class Tag_Place : TagBase
     {
         public PlaceGroup group;
@@ -20,11 +25,19 @@ namespace WSH.UI
         {
             tagMembers = tags.Where(t => t is Tag_Sensor).Select(t=>t).ToList();
             tagMembers = tagMembers.Where(t => ((Tag_Sensor)t).myPlaceGroup.Equals(group)).ToList();
-
-            for(int i = 0; i < tagMembers.Count; ++i)
+            var cm = FindObjectOfType<CM_CameraManager>();
+            var sensors = cm.sensors;
+            
+            for(int i =0;i<sensors.Length;++i)
             {
-                tagMembers[i].index = i;
+                if (tagMembers.Contains(sensors[i]))
+                {
+                    Debug.Log(i);
+                    sensors[i].index = i;
+                    continue;
+                }
             }
+
             var panels = FindObjectsOfType<UI_Panel_PlaceSensorList>();
             foreach(var p in panels)
             {
